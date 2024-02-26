08510319a94cb6586764fd219a2e961e44b6c833
Matt-Yee <matt.yee9@gmail.com>
2024-02-26 00:11:59 -0500

    updated code to generate a password

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,31 @@
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener('click', writePassword);
 
-generateBtn.addEventListener('click', () => {
-  let length;
+function writePassword() {
+
+  function generatePassword(length, lowercase, uppercase, numbers, symbols) {
+    let allowedChars = "";
+    if (lowercase) allowedChars += "abcdefghijklmnopqrstuvwxyz";
+    if (uppercase) allowedChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (numbers) allowedChars += "0123456789";
+    if (symbols) allowedChars += "~!@#$%^&*())_+,./;";
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * allowedChars.length);
+      password += allowedChars[randomIndex];
+    }
+    return password;
+  }
+
+  let length; 
   do {
-      const lengthInput = prompt('Enter a password length between 8 and 126:');
-      length = parseInt(lengthInput);
+    const lengthInput = prompt('Enter a password length between 8 and 126:');
+    length = parseInt(lengthInput);
 
-      if (length < 8 || length > 126 || isNaN(length)) {
-          alert('Please enter a valid password length between 8 and 126.');
-      }
+    if (length < 8 || length > 126 || isNaN(length)) {
+      alert('Please enter a valid password length between 8 and 126.');
+    }
   } while (length < 8 || length > 126 || isNaN(length));
 
   const lowercaseInput = prompt('Allow lowercase? (yes or no):');
@@ -27,19 +43,8 @@ generateBtn.addEventListener('click', () => {
   console.log('Uppercase:', uppercase);
   console.log('Numbers:', numbers);
   console.log('Symbols:', symbols);
-});
 
-
-
-// Write password to the #password input
-function writePassword() {
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
-
+  const password = generatePassword(length, lowercase, uppercase, numbers, symbols);
+  const passwordText = document.querySelector("#password");
   passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-
-
+};
